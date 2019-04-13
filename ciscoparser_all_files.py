fbe2448bcf42bca181bf02679bae629c51e7ac6d
vincent schuele <vinceschuele@gmail.com>
2019-04-13 13:05:17 -0500

    Updated file to output to JSON and store data in a dictionary based off hostname

--- ciscoparser_all_files.py
+++ ciscoparser_all_files.py
@@ -1,11 +1,12 @@
 import os
 import re
+import json
 from ciscoconfparse import CiscoConfParse
 from ciscoconfparse.ccp_util import IPv4Obj
 
 #update to appropriate directory
 
-dir = '/Users/vince/Desktop/Lab'
+directory = '/Users/vince/Desktop/Lab'
 
 #f = open(file, 'r')
 #file_contents = f.read()
@@ -16,17 +17,36 @@ dir = '/Users/vince/Desktop/Lab'
 
 IPv4_REGEX = r"ip\saddress\s(\S+\s+\S+)"
 
-for file in os.listdir(dir):
+outputs = 'output.txt'
+
+interface_info = {}
+interface_name = str()
+desc = str()
+ipv4_add = str()
+interfaces = []
+general_info = {}
+
+for file in os.listdir(directory):
         if file.endswith('.rtf'):
                 #load config into parser
                 parse = CiscoConfParse(file)
+                hostname = parse.find_lines(r'^hostname')
                 for intf in parse.find_objects(r'^interface'):
-                        print(str(intf.text))
-                        ip_address = intf.re_match_iter_typed(IPv4_REGEX)
-                        print(ip_address)
-                        description = intf.re_search_children(r"^\s+description")
-                        print(description)
+                        interfaces.append(str(intf.text))
+                        interface_info[str(intf.text)]=(str(intf.re_search_children(r"^\s+description")),str(intf.re_match_iter_typed(IPv4_REGEX)))
+                        #print(str(intf.text))
+			#ip_address = intf.re_match_iter_typed(IPv4_REGEX)
+                        #print(ip_address)
+                        #description = intf.re_search_children(r"^\s+description")
+                        #print(description)
+                general_info[str(hostname)]=interface_info
+                interface_info = {}
+#                version = parse.find_objects(r'^version')
+#                print(version)
+#print(interfaces)
+#print(interface_info)
+#print(general_info)
 
-                version = parse.find_objects(r'^version')
-                print(version)
+with open('device_info.json', 'w') as handle:
+     json.dump(general_info, handle, indent=4)
 
