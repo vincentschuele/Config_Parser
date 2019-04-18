27768e7ffb101a8cdf3cc65db55dac7d291e21d6
vincent schuele <vinceschuele@gmail.com>
2019-04-18 17:27:45 -0500

    continued with output cleanup

--- config_parser.py
+++ config_parser.py
@@ -47,29 +47,33 @@ def prefix_conversion(ip):
      return prefix
 
 
-for file in os.listdir(directory):
-        if file.endswith('.rtf'):
+for files in os.listdir(directory):
+        if files.endswith('.rtf'):
                 #load config into parser
-                parse = CiscoConfParse(file)
-                hostname = parse.find_lines(r'^hostname')
+                parse = CiscoConfParse(files)
+                hostname = str(parse.find_lines(r'^hostname'))
+                myhost = hostname.split()[-1].split('\\')[0]
                 for intf in parse.find_objects(r'^interface'):
-                        interfaces.append(str(intf.text))
-                        description= intf.re_search_children(r"^\s+description")#returns list of objects, cannot index[0]
+                        interface = str(intf.text)
+                        clean_interface = interface.split()[-1].replace('\\','')
+                        interfaces.append(clean_interface)
+                        description = intf.re_search_children(r"^\s+description")#returns list of objects, cannot index[0]
+                        
                         for i in description:#loop through objects
                             #print(i)
-                            new = i.re_sub(r'<IOSCfgLine\s\#\s\d+\s+\'\s',r'') #remove junk before description TEST
-                            new = new[1:].replace('\\','')#remove space
-                            #print(new, "This is new")
+                            desc = i.re_sub(r'<IOSCfgLine\s\#\s\d+\s+\'\s',r'') #remove junk before description TEST
+                            desc = desc[1:].replace('\\','')#remove space
+                            
                         ip_info = str(intf.re_match_iter_typed(IPv4_REGEX))
 #                        print(ip_info)
-                        interface_info[str(intf.text)]=(str(new),str(prefix_conversion(ip_info)))
+                        interface_info[clean_interface]=(str(desc),str(prefix_conversion(ip_info)))
                         #print(str(intf.text))
                         new = str()
 			#ip_address = intf.re_match_iter_typed(IPv4_REGEX)
                         #print(ip_address)
                         #description = intf.re_search_children(r"^\s+description")
                         #print(description)
-                general_info[str(hostname)]=interface_info
+                general_info[str(myhost)]=interface_info
                 interface_info = {}
 #                version = parse.find_objects(r'^version')
 #                print(version)
@@ -80,20 +84,6 @@ for file in os.listdir(directory):
 with open('device_info.json', 'w') as handle:
      json.dump(general_info, handle, indent=4)
 
-#csv_columns = ['Hostname','description','ip address and mask']
-#csv_file = 'test_output.csv'
-
-#with open(csv_file, 'w') as csvfile:
-#     writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
-#     for k,v in general_info.items():
-#          writer.writerow(v)
-
-#with open(outputs, 'w') as f:
-
-#     for key, value in sorted(general_info.items()):
-#          for keys, values in value.items():
-#               f.write("{} {} {} {}\n".format(key.replace('\\',''),keys.replace('\\',''),values[0],values[1].replace("\\","").split(' ')))
-   
 workbook = xlsxwriter.Workbook('Test_Output.xlsx')
 worksheet = workbook.add_worksheet()
 row = 0
@@ -108,22 +98,22 @@ with open(outputs, 'w') as f:
   
     for host in sorted( general_info):
       # cleanup hostname
-           myhost = host.split()[-1].split('\\')[0]
+      #     myhost = host.split()[-1].split('\\')[0]
       # check for empty hostname
-           if myhost:
-               for interface, ip in general_info[host].items():
-                   desc = []
-                   clean_interface = interface.split()[-1].replace('\\','')
-                   desc = ip[0]
-                   if len(desc)==0:
-                       desc = "No Description"
-                   prefix = ip[1]
-                   f.write("ROUTER {}: {} : {} : address {}\n".format(myhost, clean_interface, desc, prefix))
+#           if myhost:
+          for interface, ip in general_info[host].items():
+              desc = []
+#                   clean_interface = interface.split()[-1].replace('\\','')
+              desc = ip[0]
+              if len(desc)==0:
+                  desc = "No Description"
+              prefix = ip[1]
+              f.write("ROUTER {}: {} : {} : address {}\n".format(host, interface, desc, prefix))
           
-                   worksheet.write(row, col, myhost)
-                   worksheet.write(row, col + 1, clean_interface)
-                   worksheet.write(row, col + 2, desc)
-                   worksheet.write(row, col + 3, str(prefix))
-                   row += 1
+              worksheet.write(row, col, host)
+              worksheet.write(row, col + 1, clean_interface)
+              worksheet.write(row, col + 2, desc)
+              worksheet.write(row, col + 3, str(prefix))
+              row += 1
 workbook.close()                   
 f.close()
